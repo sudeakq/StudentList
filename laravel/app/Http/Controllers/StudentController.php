780decc14611a7f2b4cf11f36ce89221586d9b24
sudeakq <sudeakq@gmail.com>
2024-07-12 13:38:37 +0300

    added vite + react

--- laravel/app/Http/Controllers/StudentController.php
+++ laravel/app/Http/Controllers/StudentController.php
@@ -24,7 +24,7 @@ class StudentController extends Controller
   
     public function show(string $id)
     {
-     $student = $this->student->find($id);  
+     return $student = $this->student->find($id);  
     }
 
     public function update(Request $request, string $id)
